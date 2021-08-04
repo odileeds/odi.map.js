c9742ecd7aaf217ff3f94847c5f340cb652bf348
slowe <gravatar@strudel.org.uk>
2021-08-04 23:43:28 +0100

    Add popups to overlays

--- odi.map.overlay.js
+++ odi.map.overlay.js
@@ -1,7 +1,7 @@
 /**
   ODI Leeds Tiny Slippy Map
-  Plugin for GeoJSON
-  Version 0.1.1
+  Plugin for overlays (e.g. GeoJSON)
+  Version 0.1.2
 **/
 // jshint esversion: 6
 (function(root){
@@ -21,20 +21,55 @@
 	if(ODI.map){
 		var ns = 'http://www.w3.org/2000/svg';
 		ODI.map.prototype.register('overlay',{
-			'version':'0.1.1',
+			'version':'0.1.2',
 			'exec':function(_map){
+				function Thing(feature,m){
+					this.bindPopup = function(txt){
+						this._popuptext = txt;
+						if(feature.marker) this._el = feature.marker._el.querySelector('path');
+						else if(feature.svg) this._el = feature.svg;
+						ODI._addEvent('click',this._el,{'popup':txt,el:this._el,this:this},function(e){
+							var popup = m.panes.p.popup.el.querySelector('.odi-map-popup');
+							if(popup) popup.parentNode.removeChild(popup);
+							this._popup = document.createElement('div');
+							this._popup.classList.add('odi-map-popup');
+							m.panes.p.popup.el.appendChild(this._popup);
+							this._popup.innerHTML = '<div class="odi-map-popup-inner">'+e.data.popup+'</div>';
+							this.setPosition();
+						});
+						return this;
+					};
+					this.setPosition = function(){
+						if(this._popup){
+							var bb = this._el.getBoundingClientRect();
+							var bbo = m.panes.p.popup.el.getBoundingClientRect();
+							ODI._setAttr(this._popup,{'style':'top:'+(bb.top-bbo.top)+'px;left:'+(bb.left-bbo.left+bb.width/2)+'px;'});
+						}
+					};
+					return this;
+				}
 				class marker extends _map.Layer {
 					constructor(ll,attr,pane){
 						super(attr,pane);
 						this._ll = ll||ODI.map.LatLon();
 						this._el = svgEl('svg');
 						this._el.innerHTML = attr.svg;
-						ODI._setAttr(this._el,{'width':'0','height':'0','overflow':'visible'});
+						ODI._setAttr(this._el,{'xmlns':ns,'version':'1.1','overflow':'visible','width':'0','height':'0'});
+					}
+					addTo(m){
+						this._map = m;
+						var p = this._attr.pane;
+						if(!m.panes.p[p]) return m.log('ERROR','No pane %c'+p+'%c exists.','font-style:italic;','');
+						m.panes.p[p].el.appendChild(this._el);
+						m.panes.p[p].layers.push(this);
+						this.setLatLon(this._ll);
+						return this;
 					}
 					setLatLon(ll,offset){
-						var z = this._map.getZoom();
+						var z,xy;
+						z = this._map.getZoom();
 						if(!offset) offset = this._map.getCenter().toPx(z);
-						var xy = ll.toPx(z);
+						xy = ll.toPx(z);
 						ODI._setAttr(this._el,{'class':'odi-map-marker','style':'transform:translate3d('+(xy.x-offset.x)+'px,'+(xy.y-offset.y)+'px,0)'});
 					}
 				}
@@ -43,12 +78,13 @@
 						super(attr,pane);
 						this._json = json||{};
 						this._svg = svgEl('svg');
+						this._init = false;
 
 						ODI._setAttr(this._svg,{'xmlns':ns,'version':'1.1','overflow':'visible','preserveAspectRatio':'xMinYMin meet','vector-effect':'non-scaling-stroke'});
 						this._el.appendChild(this._svg);
 					}
 					update(bounds,z){
-						var offset,f,ll,xy,nw,se,el,c,i,j,k,d,props,marker,defaults,target,attr,g;
+						var offset,f,xy,nw,se,el,c,i,j,k,d,props,defaults,attr,g;
 						// Get tile x/y of centre
 						offset = bounds.getCenter().toPx(z);
 						nw = bounds.nw.toPx(z);
@@ -63,13 +99,14 @@
 							g = this._json.features[f].geometry;
 							c = g.coordinates;
 							if(g.type=="Point"){
-								// Create the marker if needed
 								if(!this._json.features[f].marker){
+									// Create the marker if needed
 									attr = {'svg':('<path d="M 0,0 L -10.84,-22.86 A 12 12 1 1 1 10.84,-22.86 L 0,0 z" fill="%COLOUR%" fill-opacity="1"></path><ellipse cx="0" cy="-27.5" rx="4" ry="4" fill="white"></ellipse>').replace(/%COLOUR%/,props.fillColor||defaults.color)};
-									this._json.features[f].marker = ODI.map.marker(offset,attr).addTo(this._map);
+									this._json.features[f].marker = ODI.map.marker(this._map.LatLon(c[1],c[0]),attr).addTo(this._map);
+								}else{
+									// Update position
+									this._json.features[f].marker.setLatLon(this._map.LatLon(c[1],c[0]));
 								}
-								// Update position
-								this._json.features[f].marker.setLatLon(this._map.LatLon(c[1],c[0]));
 							}else if(g.type=="Polygon"){
 								if(!this._json.features[f].svg){
 									el = svgEl('path');
@@ -145,7 +182,13 @@
 								this._json.features[f].svg.innerHTML = (props.title ? '<title>'+props.title+'</title>':'');
 								ODI._setAttr(this._json.features[f].svg,props);
 							}
+							if(!this._init && typeof this._attr.onEachFeature==="function"){
+								this._json.features[f]._thing = new Thing(this._json.features[f],this._map);
+								this._attr.onEachFeature.call(this,this._json.features[f],this._json.features[f]._thing);
+							}
+							this._json.features[f]._thing.setPosition();
 						}
+						this._init = true;
 					}
 					addTo(m){
 						var w,h;
@@ -169,8 +212,8 @@
 				ODI.map.marker = function(ll,attr){
 					if(!attr) attr = {};
 					if(!attr.pane) attr.pane = 'marker';	// default pane
-					return new marker(ll,attr)
-				}
+					return new marker(ll,attr);
+				};
 			}
 		});
 	}
